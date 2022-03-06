c1b73b8879cb8c59020ac8df6a7334afb00973d4
kritikapat <96338775+kritikapat@users.noreply.github.com>
2022-03-06 19:08:01 +0530

    Add files via upload

---
+++ mar2/galleryrecreate.js
@@ -0,0 +1,53 @@
+//On the gallery, add the like/dislike functionalty. --> Each image will have it's own counter.
+
+
+
+function liked1() {
+    var liked1 = document.getElementById("like1");
+    liked1.innerText = parseInt(liked1.innerText) + 1;
+}
+
+function disliked1() {
+    var disliked1 = document.getElementById("dislike1");
+    disliked1.innerText = parseInt(disliked1.innerText) + 1;
+}
+function liked2() {
+    var liked2 = document.getElementById("like2");
+    liked2.innerText = parseInt(liked2.innerText) + 1;
+}
+function disliked2() {
+    var disliked2 = document.getElementById("dislike2");
+    disliked2.innerText = parseInt(disliked2.innerText) + 1;
+}
+function liked3() {
+    var liked3 = document.getElementById("like3");
+    liked3.innerText = parseInt(liked3.innerText) + 1;
+}
+function disliked3() {
+    var disliked3 = document.getElementById("dislike3");
+    disliked3.innerText = parseInt(disliked3.innerText) + 1;
+}
+function liked4() {
+    var liked4 = document.getElementById("like4");
+    liked4.innerText = parseInt(liked4.innerText) + 1;
+}
+function disliked4() {
+    var disliked4 = document.getElementById("dislike4");
+    disliked4.innerText = parseInt(disliked4.innerText) + 1;
+}
+function liked5() {
+    var liked5 = document.getElementById("like5");
+    liked5.innerText = parseInt(liked5.innerText) + 1;
+}
+function disliked5() {
+    var disliked5 = document.getElementById("dislike5");
+    disliked5.innerText = parseInt(disliked5.innerText) + 1;
+}
+function liked6() {
+    var liked6 = document.getElementById("like6");
+    liked6.innerText = parseInt(liked6.innerText) + 1;
+}
+function disliked6() {
+    var disliked6 = document.getElementById("dislike6");
+    disliked6.innerText = parseInt(disliked6.innerText) + 1;
+}
